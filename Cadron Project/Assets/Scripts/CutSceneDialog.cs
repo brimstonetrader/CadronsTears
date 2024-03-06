b880869c3037d54ed25c8895d2ceedd42ae995fc
Amelia Jones <jonesas@hendrix.edu>
2024-03-06 15:10:41 -0600

    Press E to advance in dialog
    E completes dialog or goes to next line. Clicking next finishes the dialog line if it is not done.

--- Cadron Project/Assets/Scripts/CutSceneDialog.cs
+++ Cadron Project/Assets/Scripts/CutSceneDialog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class CutSceneDialog : MonoBehaviour
 {
@@ -15,25 +16,31 @@ public class CutSceneDialog : MonoBehaviour
     public GameObject nextButton;
     public TextMeshProUGUI nexttext;
 
+    public bool speaking = false;
     public void nextClick(){
-        if(line >= lines.Length - 1)
-        {
-        GameManager.Instance.EndCutscene();
+        if(GameManager.Instance.CheckTextDone(lines[line])){
+            if(line >= lines.Length - 1)
+            {
+            GameManager.Instance.EndCutscene();
+            speaking = false;
+            }
+            else{
+                line += 1;
+                if(line == (lines.Length - 1)){
+                // set button text to done
+                nexttext.text = "Done";
+                }
+                backButton.SetActive(true);
+            // nextButton.SetActive(false);
+                GameManager.Instance.DialogShow(lines[line]); 
+                //StartCoroutine(WaitingForNext());
+                
+                
+            }
         }
         else{
-            line += 1;
-            if(line == (lines.Length - 1)){
-            // set button text to done
-            nexttext.text = "Done";
-            }
-            backButton.SetActive(true);
-           // nextButton.SetActive(false);
-            GameManager.Instance.DialogShow(lines[line]); 
-            //StartCoroutine(WaitingForNext());
-            
-            
-        }
-        
+            GameManager.Instance.FinishText(lines[line]);
+        }    
     }
     public void backClick(){
         if(line > 0){
@@ -53,14 +60,25 @@ public class CutSceneDialog : MonoBehaviour
         backButton.SetActive(false);
         charactername.text = name;
         characterportrait.GetComponent<Image>().overrideSprite = portrait;
+        speaking = true;
+        StartCoroutine(WaitingForNext());
     }
 
-    //IEnumerator WaitingForNext()
-    //{
-       // yield return new WaitForSeconds(1);
-        //nextButton.SetActive(true);
-        //backButton.SetActive(true);
-    //}
+    IEnumerator WaitingForNext()
+    {
+        while(speaking){
+            if(Input.GetKeyDown(KeyCode.E)){
+                if(GameManager.Instance.CheckTextDone(lines[line])){
+                    nextClick();
+                }
+                else{
+                    GameManager.Instance.FinishText(lines[line]);
+                }
+            }
+            yield return null;
+        }
+       
+    }
     
     // Start is called before the first frame update
     void Start()
